da33d90aa3fee61e0fe65d9087eafac49e1e179a
jyq <jyq311601@qq.com>
2025-12-11 17:13:12 +0800

    search

--- car.h
+++ car.h
@@ -2,6 +2,13 @@
 void add ();
 void delete(int address);
 double  get_money(int time);
-void search(int addresss);
+void search(int addresss){
+     for(int i = 0;i < car_count;i++){
+         if(car[i].address == address){
+	   printf("%s\n%d\n%.lf\n%d", car[i].num, car[i].time, car[i].money, car[i].address);
+	 }
+	 break;
+     }
+}
 void desplay_all();
 
